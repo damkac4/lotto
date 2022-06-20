7dcb2a12e4d8401fe9dd52a159906fd7ca021155
dkacp <damkac4@st.amu.edu.pl>
2022-06-20 23:55:36 +0200

    Lotto

--- src/Lotto.java
+++ src/Lotto.java
@@ -10,13 +10,6 @@ public class Lotto {
     private ArrayList<Integer> numbersRandom;
     private int reward;
 
-    public int getReward() {
-        return reward;
-    }
-
-    public void setReward(int reward) {
-        this.reward = reward;
-    }
 
     public Lotto() throws IOException {
         numbersChoosen = new ArrayList<>();
@@ -30,14 +23,21 @@ public class Lotto {
         Scanner input = new Scanner(System.in);
         int number;
         System.out.println("\nEnter 6 numbers: ");
+
         for (int i = 0; i < 6; i++){
 
-            number = input.nextInt();
-            if(number<=49 && number>=1 && !numbersChoosen.contains(number)) numbersChoosen.add(number);
-            else{
-                i--;
-                System.out.println("Number beyond of bounds or already entered!");
+            while(!input.hasNextInt()) {
+                System.out.println("To nie liczba pajacu XD");
+                input.next();
             }
+
+                number = input.nextInt();
+
+                if (number <= 49 && number >= 1 && !numbersChoosen.contains(number)) numbersChoosen.add(number);
+                else{
+                    i--;
+                    System.out.println("Number beyond of bounds or already entered!");
+                }
         }
 
         System.out.println("Your numbers: ");
@@ -62,7 +62,6 @@ public class Lotto {
         }
     }
 
-
     public void check() throws IOException {
         int checked = 0;
 
@@ -74,4 +73,11 @@ public class Lotto {
         this.setReward(checked * 10);
     }
 
+    public int getReward() {
+        return reward;
+    }
+
+    public void setReward(int reward) {
+        this.reward = reward;
+    }
 }
